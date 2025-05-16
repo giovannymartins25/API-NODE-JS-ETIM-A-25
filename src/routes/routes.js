aeb975b604031cf39e19d1a8dc9d9cfe9cc81642
Giovanny Gabryel Martins de Jesus <neusa2554@gmail.com>
2025-05-16 10:47:16 -0300

    16_5

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,25 +1,8 @@
 const express = require('express'); 
 const router = express.Router(); 
 
-const AgricultoresController = require('../controllers/Agricultores'); 
-const AmendoinsController = require('../controllers/Amendoins'); 
-const OfertasController = require('../controllers/Ofertas'); 
+const RotasGiovanny = require ('/.routesGiovanny');
 
-
-router.get('/Agricultores', AgricultoresController.listarAgricultores); 
-router.post('/Agricultores', AgricultoresController.cadastrarAgricultores); 
-router.patch('/Agricultores/:id', AgricultoresController.editarAgricultores); 
-router.delete('/Agricultores/:id', AgricultoresController.apagarAgricultores); 
-
-router.get('/Amendoins', AmendoinsController.listarAmendoins); 
-router.post('/Amendoins', AmendoinsController.cadastrarAmendoins); 
-router.patch('/Amendoins/:id', AmendoinsController.editarAmendoins); 
-router.delete('/Amendoins/:id', AmendoinsController.apagarAmendoins); 
-
-router.get('/Ofertas', OfertasController.listarOfertas); 
-router.post('/Ofertas', OfertasController.cadastrarOfertas); 
-router.patch('/Ofertas/:id', OfertasController.editarOfertas); 
-router.delete('/Ofertas/:id', OfertasController.apagarOfertas); 
-
+router.use('/', RotasGiovanny);
 
 module.exports = router;
